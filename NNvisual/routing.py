aead81ac5937d99c3f7b7e097b1a480bf4b10980
Arsh0212 <114242782+Arsh0212@users.noreply.github.com>
2025-09-23 21:27:54 +0530

    Update routing.py

--- NNvisual/routing.py
+++ NNvisual/routing.py
@@ -3,5 +3,8 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/training/', consumers.NeuralNetworkConsumer.as_asgi()),
+    re_path(r'ws/training/main/', consumers.NeuralNetworkConsumer.as_asgi()),
+    re_path(r'ws/training/metrics/', consumers.NeuralNetworkConsumer.as_asgi()),
+    re_path(r'ws/training/graph/', consumers.NeuralNetworkConsumer.as_asgi()),
     # Add more websocket routes as needed
 ]
